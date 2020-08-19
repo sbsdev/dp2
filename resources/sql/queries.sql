597f76b463ee36ab6a5f42add3bfa796e74534d9
Christian Egli <christian.egli@sbs.ch>
2020-08-19 13:26:21 +0200

    Add REST interfaces for documents and global and local words

--- resources/sql/queries.sql
+++ resources/sql/queries.sql
@@ -1,21 +1,66 @@
 -- :name create-user! :! :n
--- :doc creates a new user record
+-- :doc create a new user record
 INSERT INTO users
 (id, first_name, last_name, email, pass)
 VALUES (:id, :first_name, :last_name, :email, :pass)
 
 -- :name update-user! :! :n
--- :doc updates an existing user record
+-- :doc update an existing user record
 UPDATE users
 SET first_name = :first_name, last_name = :last_name, email = :email
 WHERE id = :id
 
 -- :name get-user :? :1
--- :doc retrieves a user record given the id
+-- :doc retrieve a user record given the id
 SELECT * FROM users
 WHERE id = :id
 
 -- :name delete-user! :! :n
--- :doc deletes a user record given the id
+-- :doc delete a user record given the id
 DELETE FROM users
 WHERE id = :id
+
+---------------
+-- Documents --
+---------------
+
+-- :name get-documents :? :*
+-- :doc retrieve all documents given a limit and an offset
+SELECT * FROM documents_document LIMIT :limit OFFSET :offset
+
+-- :name get-document :? :1
+-- :doc retrieve a document record given the id
+SELECT * FROM documents_document
+WHERE id = :id
+
+-- :name get-images :? :*
+-- :doc retrieve the images for a given document id
+SELECT * FROM documents_image
+WHERE document_id = :id
+
+-----------
+-- Words --
+-----------
+
+-- :name get-global-words :? :*
+-- :doc retrieve global words given a limit and an offset
+SELECT * FROM dictionary_globalword
+LIMIT :limit OFFSET :offset
+
+-- :name get-global-word :? :*
+-- :doc retrieve global words for a given untranslated
+SELECT * FROM dictionary_globalword
+WHERE untranslated = :untranslated
+
+-- :name search-global-words :? :*
+-- :doc retrieve all global words given a simple pattern for untranslated, an optional grade, an optional type, a limit and an offset
+SELECT * FROM dictionary_globalword
+WHERE untranslated LIKE :search
+--~ (when (:grade params) "AND grade = :grade")
+--~ (when (:type params) "AND type = :type")
+LIMIT :limit OFFSET :offset
+
+-- :name get-local-words :? :*
+-- :doc retrieve local words for a given document id
+SELECT * FROM dictionary_localword
+WHERE document_id = :id
--- src/clj/dp2/routes/services.clj
+++ src/clj/dp2/routes/services.clj
@@ -1,5 +1,6 @@
 (ns dp2.routes.services
   (:require
+    [dp2.db.core :as db]
     [reitit.swagger :as swagger]
     [reitit.swagger-ui :as swagger-ui]
     [reitit.ring.coercion :as coercion]
@@ -7,6 +8,7 @@
     [reitit.ring.middleware.muuntaja :as muuntaja]
     [reitit.ring.middleware.multipart :as multipart]
     [reitit.ring.middleware.parameters :as parameters]
+    [spec-tools.data-spec :as spec]
     [dp2.middleware.formats :as formats]
     [dp2.middleware.exception :as exception]
     [ring.util.http-response :refer :all]
@@ -49,43 +51,63 @@
 
    ["/ping"
     {:get (constantly (ok {:message "pong"}))}]
-   
 
-   ["/math"
-    {:swagger {:tags ["math"]}}
+   ["/documents"
+    {:swagger {:tags ["documents"]}}
 
-    ["/plus"
-     {:get {:summary "plus with spec query parameters"
-            :parameters {:query {:x int?, :y int?}}
-            :responses {200 {:body {:total pos-int?}}}
-            :handler (fn [{{{:keys [x y]} :query} :parameters}]
+    [""
+     {:get {:summary "Get all documents"
+            :description "Get all documents. Optionally limit the result set using `limit` and `offset`."
+            :parameters {:query {(spec/opt :limit) int?, (spec/opt :offset) int?}}
+            :handler (fn [{{{:keys [limit offset] :or {limit 200 offset 0}} :query} :parameters}]
                        {:status 200
-                        :body {:total (+ x y)}})}
-      :post {:summary "plus with spec body parameters"
-             :parameters {:body {:x int?, :y int?}}
-             :responses {200 {:body {:total pos-int?}}}
-             :handler (fn [{{{:keys [x y]} :body} :parameters}]
-                        {:status 200
-                         :body {:total (+ x y)}})}}]]
+                        :body (db/get-documents {:limit limit :offset offset})})}}]
 
-   ["/files"
-    {:swagger {:tags ["files"]}}
+    ["/:id"
+     {:get {:summary "Get a document by ID"
+            :parameters {:path {:id int?}}
+            :handler (fn [{{{:keys [id]} :path} :parameters}]
+                       (if-let [doc (db/get-document {:id id})]
+                         {:status 200 :body doc}
+                         {:status 404}))}}]
 
-    ["/upload"
-     {:post {:summary "upload a file"
-             :parameters {:multipart {:file multipart/temp-file-part}}
-             :responses {200 {:body {:name string?, :size int?}}}
-             :handler (fn [{{{:keys [file]} :multipart} :parameters}]
-                        {:status 200
-                         :body {:name (:filename file)
-                                :size (:size file)}})}}]
+    ["/:id/images"
+     {:get {:summary "Get all images for a given document"
+            :parameters {:path {:id int?}}
+            :handler (fn [{{{:keys [id]} :path} :parameters}]
+                       (if-let [doc (db/get-images {:id id})]
+                         {:status 200 :body doc}
+                         {:status 404}))}}]]
 
-    ["/download"
-     {:get {:summary "downloads a file"
-            :swagger {:produces ["image/png"]}
-            :handler (fn [_]
-                       {:status 200
-                        :headers {"Content-Type" "image/png"}
-                        :body (-> "public/img/warning_clojure.png"
-                                  (io/resource)
-                                  (io/input-stream))})}}]]])
+   ["/words"
+    {:get {:summary "Get global words. Optionally filter the results by using a search string, a grade, a type and a limit and an offset."
+           :tags ["words"]
+           :parameters {:query {(spec/opt :search) string?
+                                (spec/opt :grade) int?
+                                (spec/opt :type) int?
+                                (spec/opt :limit) int?
+                                (spec/opt :offset) int?}}
+           :handler (fn [{{{:keys [search grade type limit offset]
+                            :or {limit 200 offset 0}} :query} :parameters}]
+                      {:status 200
+                       :body (db/search-global-words {:search search :grade grade :type type
+                                                      :limit limit :offset offset})})}}]
+
+   ["/words/:untranslated"
+    {:get {:summary "Get global words by untranslated"
+           :tags ["words"]
+           :parameters {:path {:untranslated string?}}
+           :handler (fn [{{{:keys [untranslated]} :path} :parameters}]
+                      (if-let [doc (db/get-global-word {:untranslated untranslated})]
+                        {:status 200 :body doc}
+                        {:status 404}))}}]
+
+   ["/documents/:id/words"
+    {:get {:summary "Get all local words for a given document"
+           :tags ["words"]
+           :parameters {:path {:id int?}}
+           :handler (fn [{{{:keys [id]} :path} :parameters}]
+                      (if-let [doc (db/get-local-words {:id id})]
+                        {:status 200 :body doc}
+                        {:status 404}))}}]
+   ])
