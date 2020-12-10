c0634e3e11815a1d66bfb98db150a5a719a7637b
Christian Egli <christian.egli@sbs.ch>
2020-12-10 11:19:56 +0100

    Display the hyphenation field for unknown only if non-null

--- src/cljs/dp2/words/unknown.cljs
+++ src/cljs/dp2/words/unknown.cljs
@@ -127,7 +127,7 @@
 
 (defn word [id]
   (let [grade @(rf/subscribe [::grade/grade])
-        {:keys [uuid untranslated uncontracted contracted type homograph-disambiguation]} @(rf/subscribe [::word id])]
+        {:keys [uuid untranslated uncontracted contracted type homograph-disambiguation hyphenated]} @(rf/subscribe [::word id])]
     [:tr
      [:td untranslated]
      (when (#{0 1} grade)
@@ -138,7 +138,8 @@
        (if contracted
          [:td [input-field uuid :contracted validation/braille-valid?]]
          [:td]))
-     [:td [input-field uuid :hyphenated #(validation/hyphenation-valid? % untranslated)]]
+     [:td (when hyphenated
+            [input-field uuid :hyphenated #(validation/hyphenation-valid? % untranslated)])]
      [:td {:width "8%"} (get words/type-mapping type "Unknown")]
      [:td {:width "8%"} homograph-disambiguation]
      [:td [local-field uuid]]
