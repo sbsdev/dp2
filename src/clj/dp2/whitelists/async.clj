dc5613eba253b0283407e729a668a071025887ab
Christian Egli <christian.egli@sbs.ch>
2021-01-26 14:40:16 +0100

    Drop the dependency on chime
    by using the same basic pattern for the global-dict-chan as for the
    local-dict-chan

--- src/clj/dp2/whitelists/async.clj
+++ src/clj/dp2/whitelists/async.clj
@@ -1,21 +1,23 @@
 (ns dp2.whitelists.async
   "Asynchronous interface to writing whitelist tables. Refer
   to [[dp2.whitelists.tables]] for the synchronous API."
-  (:require [chime.core :as chime]
-            [chime.core-async :refer [chime-ch]]
-            [clojure.core.async :as async :refer [<! >! >!! go go-loop]]
+  (:require [clojure.core.async :as async
+             :refer [<! >! >!! alts! buffer chan close! go go-loop timeout]]
             [dp2.whitelists.tables :as tables]
-            [mount.core :refer [defstate]])
-  (:import [java.time Duration Instant]))
+            [mount.core :refer [defstate]]))
 
-(def clock-interval
+(def local-clock-interval
   "Time interval at which writes to the local tables happen in miliseconds"
   (* 10 1000)) ; every 10 seconds
 
+(def global-clock-interval
+  "Time interval at which writes to the global tables happen in miliseconds"
+  (* 5 60 1000)) ; every 15 minutes
+
 (defstate local-dict-chan
-  :start (let [input-ch (async/chan (async/buffer 20))
-               clock-ch (async/chan (async/dropping-buffer 1))
-               write-ch (async/chan (async/buffer 20))]
+  :start (let [input-ch (chan (buffer 20))
+               clock-ch (chan (async/dropping-buffer 1))
+               write-ch (chan (buffer 20))]
 
            ;; throttle the write of the local tables to once every two
            ;; minutes at most by using a clock channel to trigger the
@@ -25,7 +27,7 @@
            ;; every 2 minutes. If the bucket is full the token is
            ;; dropped since the bucket channel uses a dropping buffer.
            (go (while (>! clock-ch :token)
-                 (<! (async/timeout clock-interval))))
+                 (<! (timeout local-clock-interval))))
 
            ;; the actual worker thread that writes the tables
            (go (loop []
@@ -35,42 +37,45 @@
 
            (go-loop [document-ids #{}]
              ;; listen to both the clock and the input channel
-             (let [[v ch] (async/alts! [input-ch clock-ch])]
+             (let [[v ch] (alts! [input-ch clock-ch])]
                (condp = ch
                  ;; if we get a document-id from the input channel, we
                  ;; just add it to the set of ids to export. This also
                  ;; depupes the document-ids
                  input-ch (if (some? v)
                             (recur (conj document-ids v))
-                            (do
-                              (async/close! clock-ch)
-                              (async/close! write-ch)))
+                            (doseq [c [clock-ch write-ch]] (close! c)))
                  ;; at regular intervals we get an event from the
                  ;; clock channel. Then it's time to actually do the
                  ;; export
-                 clock-ch (do
-                            (doseq [id document-ids]
-                              (>! write-ch id))
-                            (recur #{})))))
+                 clock-ch (do (doseq [id document-ids] (>! write-ch id))
+                              (recur #{})))))
            input-ch)
   :stop (when local-dict-chan
-          (async/close! local-dict-chan)))
+          (close! local-dict-chan)))
 
 (defstate global-dict-chan
-  ;; throttle the write of the global tables to once every two hours at most
-  :start (let [times (rest (chime/periodic-seq (Instant/now) (Duration/ofHours 2)))
-               cron (chime-ch times {:ch (async/chan (async/sliding-buffer 1))})
-               ch (async/chan (async/sliding-buffer 1))]
-           (go-loop []
-             (let [v (<! ch)]
-               (if (nil? v)
-                 (async/close! cron)
-                 (when-let [_ (<! cron)]
+  :start (let [input-ch (chan (async/dropping-buffer 1))
+               clock-ch (chan (async/dropping-buffer 1))]
+
+           ;; throttle the write of the global tables to once every 15
+           ;; minutes at most
+
+           ;; The clock thread.
+           (go (while (>! clock-ch :token)
+                 (<! (timeout global-clock-interval))))
+
+           ;; the actual worker thread that writes the tables
+           (go
+             (while (<! clock-ch)
+               (let [v (<! input-ch)]
+                 (if (some? v)
                    (tables/export-global-tables)
-                   (recur)))))
-           ch)
+                   (close! clock-ch)))))
+
+           input-ch)
   :stop (when global-dict-chan
-          (async/close! global-dict-chan)))
+          (close! global-dict-chan)))
 
 (defn export-local-tables [document-id]
   (>!! local-dict-chan document-id))
