b0b73e071bc1f3107cb7b489c77aa36cc1eac31a
Christian Egli <christian.egli@sbs.ch>
2021-01-26 14:36:57 +0100

    Write the global tables if a global word is added or deleted

--- src/clj/dp2/words/global.clj
+++ src/clj/dp2/words/global.clj
@@ -1,10 +1,9 @@
 (ns dp2.words.global
-  (:require
-   [clojure.set :refer [rename-keys]]
-   [dp2.db.core :as db]
-   [dp2.hyphenate :as hyphenate]
-   [dp2.words :as words]
-   [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.tools.logging :as log]
+            [dp2.db.core :as db]
+            [dp2.whitelists.async :as whitelists]
+            [dp2.words :as words]))
 
 (defn get-words [{:keys [untranslated limit offset]}]
   (-> (db/find-global-words {:untranslated (if (string/blank? untranslated) "%" untranslated)
@@ -13,11 +12,21 @@
 (def dictionary-keys [:untranslated :braille :type :grade :homograph-disambiguation])
 
 (defn put-word [word]
-  (->> word
-       words/separate-word
-       (map #(db/insert-global-word (words/to-db % dictionary-keys words/dictionary-mapping)))
-       (reduce +)))
+  (log/debug "Add global word" word)
+  (let [insertions
+        (->> word
+             words/separate-word
+             (map #(db/insert-global-word (words/to-db % dictionary-keys words/dictionary-mapping)))
+             (reduce +))]
+    (whitelists/export-global-tables)
+    insertions))
 
 (defn delete-word [word]
-  (db/delete-global-word
-   (words/to-db word [:untranslated :type :homograph-disambiguation] words/dictionary-mapping)))
+  (log/debug "Delete global word" word)
+  (let [deletions
+        (db/delete-global-word
+         (words/to-db word
+                      [:untranslated :type :homograph-disambiguation]
+                      words/dictionary-mapping))]
+    (whitelists/export-global-tables)
+    deletions))
