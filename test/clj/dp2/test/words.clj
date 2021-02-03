f2dc07568edbfd8bca5be3b92ca84049058a6930
Christian Egli <christian.egli@sbs.ch>
2021-02-03 14:54:24 +0100

    Fix the test cases
    is needs a predicate form such as (is (= 4 (+ 2 2)))

--- test/clj/dp2/test/words.clj
+++ test/clj/dp2/test/words.clj
@@ -9,20 +9,21 @@
   (testing "Unknown words"
 
     (testing "Extracting supplement hyphen words"
-      (is (extract-hyphen-words "Hallo- wie geh't heute oder -morgen aber nicht-so-schnell-")
-          #{"hallo┊" "┊morgen" "schnell┊"})
+      (is (= (extract-hyphen-words "Hallo- wie geh't heute oder -morgen aber nicht-so-schnell-")
+             #{"hallo┊" "┊morgen" "schnell┊"}))
 
-      (is (extract-hyphen-words "geh't- noch schneller")
-          #{"geht's┊"}))
+      (is (= (extract-hyphen-words "geht's- noch schneller")
+             #{"geht's┊"})))
 
     (testing "Extracting ellipsis words"
-      (is (extract-ellipsis-words "...HĘllo Leute ...wie gehts'... euch hEute...")
-          #{"┊hęllo" "┊wie" "gehts'┊" "heute┊"}))
+      (is (= (extract-ellipsis-words "...HĘllo Leute ...wie gehts'... euch hEute...")
+             #{"┊hęllo" "┊wie" "gehts'┊" "heute┊"})))
 
     (testing "Filtering special words"
-      (is (filter-special-words "...HĘllo Leute ...wie gehts'... euch hEute... wahrlich gross- äh, nein gross-artig")
-          " Leute   euch  wahrlich  äh, nein gross-artig"))
+      (is (= (filter-special-words "...HĘllo Leute ...wie gehts'... euch hEute... wahrlich gross- äh, nein gross-artig")
+             " Leute   euch  wahrlich  äh, nein gross-artig")))
 
     (testing "Extracting plain words"
-      (is (filter-special-words "...HĘllo Leute ...wie gehts'... euch hEute... wahrlich gross- äh, nein gross-artig")
-          #{"wahrlich" "gross" "artig" "euch" "leute" "nein"}))))
+      (is (= (extract-words "...HĘllo Leute ...wie gehts'... euch hEute... wahrlich gross- äh, nein gross-artig")
+             #{"wahrlich" "gross" "artig" "euch" "leute" "nein"}))
+
